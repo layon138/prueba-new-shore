7f5277283db4f044a6feec7f257f0131a0d4db82
layon-insoftar <julian.vargas@insoftar.com>
2022-11-02 21:48:51 -0500

    lista funcion buscador de vuelos

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,58 +9,77 @@ import { FlightResponse, Journey } from '../interfaces/jorney.interface';
   providedIn: 'root'
 })
 export class DataService {
-  private maxFlights=0
+  private  numberScales=0
   constructor(private http: HttpClient) {
 
   }
 
-  findTravel(where:string,todosLosVuelos:FlightResponse[],arrival:string){
-    const existeVuelos=todosLosVuelos?.filter(state => state.departureStation==where && state.arrivalStation==arrival)
-    console.log(existeVuelos)
-    if(existeVuelos.length==0){
-      let list:any[]=[]
-        const newTravel=todosLosVuelos?.filter(state => state.departureStation==where)
-        console.log(newTravel)
-        for (const element of newTravel) {
-          const existeVuelos2=todosLosVuelos?.filter(state => state.departureStation==element.arrivalStation && state.arrivalStation==arrival)
-          if(existeVuelos2.length>0){
-            list.push(element) 
-            list.push(existeVuelos2[0]) 
-            break
-          }
-        }
-        return list
-    }else{
-      return existeVuelos
+  travelRecurrency(whereIwas:string,where: string,allFlights: FlightResponse[], arrival: string){
+    const flightsToScales = allFlights?.filter(state => state.departureStation == where && state.arrivalStation!=whereIwas)
+    let listOfScales: FlightResponse[] = []
+    let scaleBet: FlightResponse[] = []
+    for (const possibleScale of flightsToScales) {
+      const flightToDestination = allFlights?.filter(state => state.departureStation == possibleScale.arrivalStation && state.arrivalStation == arrival)
+      if (flightToDestination.length > 0) {
+        listOfScales.push(possibleScale)
+        listOfScales.push(flightToDestination[0])
+        break
+      }
+      this.numberScales++
+      if( this.numberScales==4){
+        break
+      }
+      const newList=this.travelRecurrency(possibleScale.departureStation,possibleScale.arrivalStation,allFlights,arrival)
+     if(newList.length>0){
+      listOfScales.push(possibleScale)
+      newList.forEach(scale => {
+        listOfScales.push(scale)
+      });
+      break
+     }
     }
-    
+    return listOfScales
   }
 
-  getJorney(jorney:Journey){
-    return this.http.get<any>(`${environment.APIDomain}api/flights/2`,{}).pipe(
-      map((flights:FlightResponse[]) => {
-        const list:FlightResponse[]=this.findTravel(jorney.origin,flights,jorney.destination)
+  findTravel(where: string, allFlights: FlightResponse[], arrival: string) {
+    const flightToDestination = allFlights?.filter(state => state.departureStation == where && state.arrivalStation == arrival)
+    console.log(flightToDestination)
+    if (flightToDestination.length == 0) {
+      let list: any[] = []
+      this.numberScales=0
+      list =this.travelRecurrency("",where, allFlights,arrival)
+      return list
+    } else {
+      return flightToDestination
+    }
+
+  }
+
+  getJorney(jorney: Journey) {
+    return this.http.get<any>(`${environment.APIDomain}api/flights/2`, {}).pipe(
+      map((flights: FlightResponse[]) => {
+        const list: FlightResponse[] = this.findTravel(jorney.origin, flights, jorney.destination)
         console.log(list)
-       list.forEach(element => {
-        jorney.price=jorney.price+element.price
-        jorney.flights.push(
-          {
-            destination:element.arrivalStation,
-            origin:element.departureStation,
-            price:element.price,
-            transport:{
-              flightCarrier:element.flightCarrier,
-              flightNumber:element.flightNumber
+        list.forEach(element => {
+          jorney.price = jorney.price + element.price
+          jorney.flights.push(
+            {
+              destination: element.arrivalStation,
+              origin: element.departureStation,
+              price: element.price,
+              transport: {
+                flightCarrier: element.flightCarrier,
+                flightNumber: element.flightNumber
+              }
             }
-          }
-        )
+          )
         });
         return jorney
-      } )
+      })
     );
   }
 
-  
+
 
 
 }
